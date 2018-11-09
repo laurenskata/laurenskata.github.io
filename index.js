64783a65d2dcf82dec0048c46d91cb8c265379e8
Laurens Verhulst <lajmverhulst@gmail.com>
2018-11-09 10:19:56 +0000

    Adds new sections

--- index.js
+++ index.js
@@ -39,3 +39,19 @@ document.addEventListener('DOMContentLoaded', function () {
     return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
   }
 });
+function copyToClipboard () {
+  /* Get the text field */
+  var el = document.createElement('textarea');
+  // Set value (string to be copied)
+  el.value = 'lajmverhulst@gmail.com';
+  // Set non-editable to avoid focus and move outside of view
+  el.setAttribute('readonly', '');
+  el.style = {position: 'absolute', left: '-9999px'};
+  document.body.appendChild(el);
+  // Select text inside element
+  el.select();
+  // Copy text to clipboard
+  document.execCommand('copy');
+  // Remove temporary element
+  document.body.removeChild(el);
+}
